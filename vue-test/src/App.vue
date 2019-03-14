5f0d2c88ba9c62ae27eab611a035a77440659743
690167690@qq.com <1123279616@qq.com>
2019-03-14 20:50:57 +0800

    vue-cli，js环境搭建，演练。

--- vue-test/src/App.vue
+++ vue-test/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/download">Download</router-link> |
+      <router-link to="/123">Redirect</router-link>
     </div>
     <router-view/>
   </div>
---
+++ vue-test/src/views/Download.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="download">
+        这是下载页面。
+    </div>
+</template>
