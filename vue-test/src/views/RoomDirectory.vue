df4bba4d7b12eb43eb9044830e89c788535a70ab
690167690@qq.com <1123279616@qq.com>
2019-03-15 17:16:52 +0800

    vue-cli:删除注释代码

--- vue-test/src/views/RoomDirectory.vue
+++ vue-test/src/views/RoomDirectory.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="room-directory">
     <p>房间列表</p>
-    <!-- <ul>
-      <li v-for="(item, index) in list" :key="index+1">
-        {{item}}
-      </li>
-    </ul> -->
     <div id="nav">
       <div v-for="(item, index) in list" :key="index+1">
         <router-link :to="'/'+item">{{item+" "}}</router-link>
@@ -16,7 +11,6 @@
 </template>
 
 <script>
-// import Com
 export default {
   name: 'roomDirectory',
   data: function () {
