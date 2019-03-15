a4dbc3646b1a75ece0abea72da4240a79be28e23
690167690@qq.com <1123279616@qq.com>
2019-03-15 08:16:51 +0800

    vue-cli: import语句后面不能跟分号

--- vue-test/src/router.js
+++ vue-test/src/router.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
-import Download from './views/Download.vue';
+import Download from './views/Download.vue'
 
 Vue.use(Router)
 
