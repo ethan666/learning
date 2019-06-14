32e19f5b58b485fa93554e754b01b1c6be34c2c1
690167690@qq.com <1123279616@qq.com>
2019-06-14 17:32:13 +0800

    js 学习

---
+++ html/js/declare.js
@@ -0,0 +1,25 @@
+// var
+
+// var b = 1;
+
+function abc(){
+  if(true){
+    var b = 2;
+  }else{
+    var b = 3
+  }
+  console.log(b)
+}
+
+
+//const 
+
+// const co = 3
+function coFuc(){
+  if(true){
+    const co = 2;
+  }else{
+    const co = 1;
+  }
+  console.log(co)
+}
