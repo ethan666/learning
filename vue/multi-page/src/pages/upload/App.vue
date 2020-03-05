b469cc488459e524fdcadeea9fae90ee08b9ac43
tanyiyang <690167690@qq.com>
2020-03-05 14:06:24 +0800

    multi-page

---
+++ vue/multi-page/src/pages/upload/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <a href="/" target="home">home</a>
+        <a href="/about" target="home">about</a>
+    </div>
+</template>
+
+<style lang="less" scoped>
+a{
+    margin-right: 10px;
+    &:last-child{
+        margin-right: 0px;
+    }
+}
+</style>
