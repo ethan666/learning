e267d0a20b83056555d3a37eaf34801d622baed8
ethan666 <690167690@qq.com>
2021-10-14 17:28:18 +0800

    js 工具方法

--- html/js/networkUtil.js
+++ html/js/networkUtil.js
@@ -7,4 +7,18 @@ const isLocalhost = () => Boolean(
         /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
       )
   )
+
+export function getQueryVariable(variable){
+  const index = window.location.href.indexOf('?')
+  const query = window.location.href.substring(index+1)
+  var vars = query.split("&");
+  for (var i=0;i<vars.length;i++) {
+    var pair = vars[i].split("=");
+    if(pair[0] == variable){
+      return pair[1];
+    }
+  }
+  return false;
+}
   
+export default { isLocalhost }
