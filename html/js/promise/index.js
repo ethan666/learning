d90681037c1ec0f43d5b8fa7bf8160b78602c4ed
ethan666 <690167690@qq.com>
2022-01-26 15:42:09 +0800

    css 测试用例

--- html/js/promise/index.js
+++ html/js/promise/index.js
@@ -1,46 +1,58 @@
-const promise = new Promise(function(resolve, reject){
-    setTimeout(()=>{
-        var a = 0
-        if(a>1){
-            resolve({status:'success', a})
-        }else{
-            reject({status:'fail', a})
-        }
-    }, 1000)
-})
+/*
+ * @Descripttion: 文件描述
+ * @version: 1.0
+ * @Author: 谭义洋
+ * @Date: 2020-04-28 14:03:23
+ * @LastEditors: 谭义洋
+ * @LastEditTime: 2022-01-26 10:03:42
+ */
+const promise = new Promise(function (resolve, reject) {
+  setTimeout(() => {
+    var a = 0;
+    if (a > 1) {
+      resolve({ status: "success", a });
+    } else {
+      reject({ status: "fail", a });
+    }
+  }, 1000);
+});
 
-promise.then(
-    res => {
-        const {status, a} = res
-        console.log(`status:${status}, a:${a}`)
+promise
+  .then(
+    (res) => {
+      const { status, a } = res;
+      console.log(`status:${status}, a:${a}`);
     },
-    // res => {
-    //     const {status, a} = res
-    //     console.log(`status:${status}, a:${a}`)
-    // }
-).catch(err => console.log(`err: ${err}`))
+    (res) => {
+      const { status, a } = res;
+      console.log(`status:${status}, a:${a}`);
+    }
+  )
+  .catch((err) => console.log(`err: ${err}`));
 
-Promise.reject('fail').then(res => {console.log(`resolve:${res}`)},res => {console.log(`reject:${res}`)})
+Promise.reject("fail").then(
+  (res) => {
+    console.log(`resolve:${res}`);
+  },
+  (res) => {
+    console.log(`reject:${res}`);
+  }
+);
 
-function getProjectId (){
-    return new Promise(function(resolve, reject){
-        setTimeout(()=>{
-            resolve(100)
-        }, 1000)
-        setTimeout(()=>{
-            resolve(0)
-        }, 50)
-    })
+function getProjectId() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve(100);
+    }, 1000);
+    setTimeout(() => {
+      resolve(0);
+    }, 50);
+  });
 }
 
-async function aaa(){
-    const res = await getProjectId()
-    console.log(res)
+async function aaa() {
+  const res = await getProjectId();
+  console.log(res);
 }
 
 // aaa()
-
-
-
-
-
