60732427c26ef0c248bbfaa704270e3ecea90882
谭义洋 <690167690@qq.com>
2023-11-28 17:00:56 +0800

    feat: html

--- html/canvas/index.js
+++ html/canvas/index.js
@@ -15,12 +15,30 @@ canvas.style.height = canvas.height + 'px';
 // ctx.fillRect(10, 10, 100, 100);
 
 
-const ctx = canvas.getContext("2d");
-ctx.font='30px verdana';
+// const ctx = canvas.getContext("2d");
+// ctx.font='30px verdana';
 // ctx.textAlign='center';
 // ctx.textBaseline='middle';
 
 
-ctx.rotate(45 * Math.PI / 180);
+// ctx.rotate(45 * Math.PI / 180);
 // ctx.fillRect(50, 20, 100, 50);
-ctx.fillText('M', 250, 250)
+// ctx.fillText('M', 250, 250)
+const angle = 30 * Math.PI / 180;
+
+
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = 'green';
+// ctx.transform(1, 0, -0.5, 1, 0, 0);
+// ctx.fillRect(0, 0, 1000, 500);
+ctx.transform(1, 0, -Math.tan(angle), 1, Math.tan(angle)*250, 0);
+
+ctx.arc(500, 250, 5, 0, 2*Math.PI);
+ctx.fill();
+
+
+// ctx.transform(1, 0, 0, 1, 0, 0);
+ctx.font = `100px 微软雅黑`
+ctx.textBaseline = 'middle'
+ctx.textAlign = 'center'
+ctx.strokeText('24', 500, 250);
